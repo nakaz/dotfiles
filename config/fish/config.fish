437cf6b135f96b2fe580ee9ebb2ea752b68eddcb
Sean Nakamura <seanenakamura@gmail.com>
2020-12-27 18:43:23 -0800

    Initial fish

---
+++ config/fish/config.fish
@@ -0,0 +1,9 @@
+# NOTE: disables "Welcome to Fish" greeting
+set fish_greeting
+
+# Base16 Shell
+eval sh $HOME/.config/oceanic-next-shell/oceanic-next.dark.sh
+
+# Editor
+set -U EDITOR nvim
+alias vim="nvim"
