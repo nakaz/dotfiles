79b3c918f7fee58b717214cd6e61265f2c28a39a
Sean Nakamura <seanenakamura@gmail.com>
2020-12-28 01:15:02 -0800

    fish configs

---
+++ config/fish/conf.d/env.fish
@@ -0,0 +1,7 @@
+# Base16 Shell
+eval sh $HOME/.config/oceanic-next-shell/oceanic-next.dark.sh
+
+set g -x EDITOR nvim
+
+set -g -x GOPATH $HOME/code/go
+set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,3 @@
-# NOTE: disables "Welcome to Fish" greeting
-set fish_greeting
-
-# Base16 Shell
-eval sh $HOME/.config/oceanic-next-shell/oceanic-next.dark.sh
-
-# Editor
-set -U EDITOR nvim
-alias vim="nvim"
+for file in ~/.config/fish/conf.d/*.fish
+	source $file
+end
